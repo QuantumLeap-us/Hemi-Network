ec78f2b92c3d091045de8280dc313262e42c6bf5
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2024-12-22 22:18:39 +0800

    Update Hemi.sh

--- Hemi.sh
+++ Hemi.sh
@@ -143,7 +143,7 @@ generate_key() {
 
 # 运行节点函数
 run_node() {
-    DIRECTORY="$HOME/heminetwork_v0.6.0_linux_amd64"
+    DIRECTORY="$HOME/heminetwork_v0.8.0_linux_arm64"
 
     echo "进入目录 $DIRECTORY..."
     cd "$DIRECTORY" || { echo "目录 $DIRECTORY 不存在。"; exit 1; }
